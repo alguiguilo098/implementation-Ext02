8a90be3e8f73d08fdc22e0dbe5c23db5cf418874
alguiguilo098 <guiguigui098@gmail.com>
2025-04-18 15:22:26 -0400

    feat: inode size

--- Shell.cpp
+++ Shell.cpp
@@ -9,6 +9,7 @@
 #include <cstdio>
 
 using namespace std;
+# define BLOCK_SIZE 1024
 
 Shell::Shell(string path){
     
@@ -29,6 +30,7 @@ Shell::Shell(string path){
 
     fseek(file,block_size+sizeof(ext2_super_block),SEEK_SET);
     fread(&this->gd,sizeof(ext2_group_desc),1,file);
+    fclose(file);
 }
 
 void Shell::info()
@@ -56,9 +58,9 @@ void Shell::clear(){
     // Clear method implementation
     std::cout <<"\033[2J\033[1;1H";
 }
-void Shell::read_inode(FILE *fd, int no_inode, ext2_inode *inode) {
-    
-    fseek(fd, (no_inode-1)*gd.bg_inode_table*sb.s_inode_size,SEEK_SET);
+void Shell::read_inode(FILE *fd, int num_inode, ext2_inode *inode) {
+
+    fseek(fd, +(BLOCK_SIZE*gd.bg_inode_table)*(num_inode-1)*sizeof(ext2_inode),SEEK_SET);
     fread(inode, sizeof(ext2_inode),1,fd);
 }
 
@@ -71,9 +73,10 @@ void Shell::run()
     // Run Shell 
     std::string path = "/";
     std::string command;
-
+    ext2_inode inode;
+    
     while (true){
-
+        
         std::cout << "[" << path << "]$>";
         std::cin >> command;
         
@@ -87,7 +90,15 @@ void Shell::run()
             // clear shell 
             this->clear();
         }else if(command=="iprintf"){
-
+            FILE *fd = fopen("myext2image.img", "r");
+            read_inode(fd,2,&inode);
+            cout << "Inode number: " << 2 << endl;
+            cout << "File type: " << inode.i_mode << endl;
+            cout << "File size: " << inode.i_size << endl; 
+            cout << "Access time: " << inode.i_atime << endl;
+            cout << "Modification time: " << inode.i_mtime << endl;
+            cout << "Change time: " << inode.i_ctime << endl;
+            cout << "Links count: " << inode.i_links_count << endl;
         }
         else{
             // invalid command 
